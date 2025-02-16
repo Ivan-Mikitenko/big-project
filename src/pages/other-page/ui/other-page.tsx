905965be59fe1f2bb5de2e1abd19ab5dbc25c86e
Ivan Mikitenko <mikiten367@gmail.com>
2025-02-16 15:53:32 +0300

    16.02 added i18next plugin + bugfix

--- src/pages/other-page/ui/other-page.tsx
+++ src/pages/other-page/ui/other-page.tsx
@@ -2,13 +2,13 @@ import { useTranslation } from 'react-i18next';
 import React from 'react';
 
 const OtherPage = () => {
-	const { t } = useTranslation('other');
+  const { t } = useTranslation('other');
 
-	return (
-		<div>
-			<h1>{t('Other Page')}</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>{t('Other Page')}</h1>
+    </div>
+  );
 };
 
 export default OtherPage;
